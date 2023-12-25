ff1b82be4cd687200b574279639623bf01b828fa
namnhat0620 <94464940+namnhat0620@users.noreply.github.com>
2023-12-25 22:15:28 +0700

    update ver

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Chat-app')
     .setDescription('The chat-app API description')
-    .setVersion('1.6')
+    .setVersion('1.7')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
